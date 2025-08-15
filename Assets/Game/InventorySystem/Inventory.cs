13385e66ed158317c1e424948d00865e39eab6d7
Lucas Soares <lucassoares1793@gmail.com>
2025-08-14 23:35:22 -0300

    Write basic inventory functions for Add/Remove item, supporting stacking and leftovers

--- Assets/Game/InventorySystem/Inventory.cs
+++ Assets/Game/InventorySystem/Inventory.cs
@@ -2,38 +2,94 @@ using System;
 using UnityEngine;
 using InventoryDemo.Items;
 
-namespace InventoryDemo.Inventory
+namespace InventoryDemo.InventorySystem
 {
     public class Inventory : MonoBehaviour
     {
-        [SerializeField] private int inventoryRows = 4; 
+        [SerializeField] private int inventoryRows = 4;
         [SerializeField] private int inventoryColumns = 8;
-        
+
         private ItemData[] inventory;
 
         private void OnValidate()
         {
-            inventory = new ItemData[inventoryRows*inventoryColumns];
+            inventory = new ItemData[inventoryRows * inventoryColumns];
         }
 
         private int Slot2DTo1D(int row, int column) => row * inventoryColumns + column;
         private (int row, int col) Slot1DTo2D(int index) => (index % inventoryColumns, index / inventoryColumns);
 
-    
-
-        public bool AddItem(ItemData item)
+        private int FindFirstEmptySlot()
         {
-            foreach (ItemData itemData in inventory)
+            for (int i = 0; i < inventory.Length; i++)
             {
-                if(itemData == item)
+                ItemData item = inventory[i];
+                if (item.Data.Id <= 0) return i;
             }
-            int idx = FindFirstEmptySlot();
-            if (idx < 0) return false;
-            
-            inventory[idx] = item;
+
+            return -1;
+        }
+
+        private int FindItemIndex(ItemData item)
+        {
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                if (inventory[i] == item)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool AddItem(ItemData addedItem, out ItemData leftoverItem)
+        {
+            leftoverItem = addedItem;
+            int leftovers = addedItem.Amount;
+            int idx = FindItemIndex(addedItem);
+
+            // Item already in inventory, increment amount
+            if (idx >= 0)
+            {
+                int calculatedAmount = inventory[idx].Amount + addedItem.Amount;
+                leftovers = calculatedAmount - inventory[idx].Data.MaxStack;
+                inventory[idx].Amount = Mathf.Clamp(calculatedAmount, 0, inventory[idx].Data.MaxStack);
+            }
+
+            if (leftovers > 0)
+            {
+                idx = FindFirstEmptySlot();
+                // No more space for leftovers
+                if (idx < 0)
+                {
+                    leftoverItem.Amount = leftovers;
+                    return false;
+                }
+
+                inventory[idx] = addedItem;
+                inventory[idx].Amount = leftovers;
+            }
+
             return true;
         }
-        
-        public bool
+
+        public bool RemoveItem(ItemData removedItem, bool shouldOnlyRemoveIfAmountIsEnough = false)
+        {
+            int idx = FindItemIndex(removedItem);
+            if (idx < 0) return false;
+
+            int leftovers = inventory[idx].Amount - removedItem.Amount;
+            if (shouldOnlyRemoveIfAmountIsEnough && leftovers >= 0)
+            {
+                inventory[idx].Amount = leftovers;
+            }
+            else
+            {
+                inventory[idx].Amount = Mathf.Max(leftovers, 0);
+            }
+
+            return true;
+        }
     }
 }
