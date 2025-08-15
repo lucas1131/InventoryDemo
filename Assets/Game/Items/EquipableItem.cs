f5703073494e3a8867e9bbadfae5460b32302e1b
Lucas Soares <lucassoares1793@gmail.com>
2025-08-15 17:29:25 -0300

    Organize item content a bit

--- Assets/Game/Items/EquipableItem.cs
+++ Assets/Game/Items/EquipableItem.cs
@@ -4,14 +4,9 @@ namespace InventoryDemo.Items
 {
     public class EquipableItem : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        }
+        [SerializeField] private GameObject pivot;
+        public GameObject Pivot => pivot;
+        
+        
     }
 }
