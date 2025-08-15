320fbb6ac83fbb949e152ff04a77cec95f01fff2
Lucas Soares <lucassoares1793@gmail.com>
2025-08-15 17:31:57 -0300

    Implement activation for equippable items so they work with animation events

--- Assets/Game/Items/EquipableItem.cs
+++ Assets/Game/Items/EquipableItem.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace InventoryDemo.Items
@@ -5,8 +6,22 @@ namespace InventoryDemo.Items
     public class EquipableItem : MonoBehaviour
     {
         [SerializeField] private GameObject pivot;
+        [SerializeField] private Collider hitbox;
         public GameObject Pivot => pivot;
-        
-        
+
+        private void Awake()
+        {
+            if (hitbox != null) hitbox.enabled = false;
+        }
+
+        public void Activate()
+        {
+            hitbox.enabled = true;
+        }
+
+        public void Deactivate()
+        {
+            hitbox.enabled = false;
+        }
     }
 }
