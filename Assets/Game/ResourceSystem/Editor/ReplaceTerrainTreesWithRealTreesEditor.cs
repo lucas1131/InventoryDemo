ce64add56d495627a5bc12b3dab835d26694d0b0
Lucas Soares <lucassoares1793@gmail.com>
2025-08-16 02:38:59 -0300

    Move helper script for replacing terrain trees with real trees to Editor assembly

---
+++ Assets/Game/ResourceSystem/Editor/ReplaceTerrainTreesWithRealTreesEditor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+// This file should be in an Editor only assembly
+namespace InventoryDemo.ResourceSystem.Editor
+{
+    [CustomEditor(typeof(ReplaceTerrainTreesWithRealTrees))]
+    public class ReplaceTerrainTreesWithRealTreesEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Replace terrain trees with object trees"))
+            {
+                Replace((ReplaceTerrainTreesWithRealTrees)target);
+            }
+        }
+
+        private void Replace(ReplaceTerrainTreesWithRealTrees owner)
+        {
+            Terrain terrain = Terrain.activeTerrain;
+            TerrainData terrainData = terrain.terrainData;
+            Vector3 terrainPos = terrain.transform.position;
+
+            foreach (TreeInstance treeInstance in terrainData.treeInstances)
+            {
+                Vector3 worldPos = Vector3.Scale(treeInstance.position, terrainData.size) + terrainPos;
+                Quaternion rotation = Quaternion.Euler(0, treeInstance.rotation * 360f, 0);
+                GameObject treeObject = Instantiate(owner.TreePrefab, worldPos, rotation);
+
+                treeObject.transform.localScale = Vector3.one * treeInstance.widthScale;
+                treeObject.transform.parent = terrain.transform;
+            }
+            
+            terrainData.treeInstances = Array.Empty<TreeInstance>();
+        }
+    }
+}
--- Assets/Game/ResourceSystem/ReplaceTerrainTreesWithRealTrees.cs
+++ Assets/Game/ResourceSystem/ReplaceTerrainTreesWithRealTrees.cs
@@ -1,45 +1,9 @@
-﻿using System;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
-// This file should be in an Editor only assembly
 namespace InventoryDemo.ResourceSystem
 {
-    [CustomEditor(typeof(ReplaceTerrainTreesWithRealTrees))]
-    public class ReplaceTerrainTreesWithRealTreesEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-
-            if (GUILayout.Button("Replace terrain trees with object trees"))
-            {
-                Replace((ReplaceTerrainTreesWithRealTrees)target);
-            }
-        }
-
-        private void Replace(ReplaceTerrainTreesWithRealTrees owner)
-        {
-            Terrain terrain = Terrain.activeTerrain;
-            TerrainData terrainData = terrain.terrainData;
-            Vector3 terrainPos = terrain.transform.position;
-
-            foreach (TreeInstance treeInstance in terrainData.treeInstances)
-            {
-                Vector3 worldPos = Vector3.Scale(treeInstance.position, terrainData.size) + terrainPos;
-                Quaternion rotation = Quaternion.Euler(0, treeInstance.rotation * 360f, 0);
-                GameObject treeObject = Instantiate(owner.TreePrefab, worldPos, rotation);
-
-                treeObject.transform.localScale = Vector3.one * treeInstance.widthScale;
-                treeObject.transform.parent = terrain.transform;
-            }
-            
-            terrainData.treeInstances = Array.Empty<TreeInstance>();
-        }
-    }
-
     public class ReplaceTerrainTreesWithRealTrees : MonoBehaviour
     {
-        [SerializeField] internal GameObject TreePrefab;
+        [SerializeField] public GameObject TreePrefab;
     }
 }
