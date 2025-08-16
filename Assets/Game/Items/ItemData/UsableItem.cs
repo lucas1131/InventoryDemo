5d32e77ed6f28563fd0238f16570e0b35307ea47
Lucas Soares <lucassoares1793@gmail.com>
2025-08-16 02:15:58 -0300

    Create placeholder UsableItem object but this is just a proof of concept, no implementation

---
+++ Assets/Game/Items/ItemData/UsableItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace InventoryDemo.Items.ItemData
+{
+    public class UsableItem : ScriptableObject
+    {
+        public void Use()
+        {
+            
+        }
+    }
+}
